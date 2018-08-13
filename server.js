da34e5775df56cf9060a7a8b68080b94c7cda499
Marc-André Leclair <marc-andreleclair@Tactios-MacBook-Pro.local>
2018-08-13 19:32:44 -0400

    created initial endpoints for users

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
@@ -7,6 +9,9 @@ const profile = require('./routes/api/profile');
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 //DB config
 const db = require('./config/keys').mongoURI;
 
